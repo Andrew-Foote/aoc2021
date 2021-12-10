2c19866d9b4a23d9bc57884f9d6c6d918f73f08c
Andrew Foote <footeandrew1@gmail.com>
2021-12-10 18:50:25 +0000

    day 9 level 2

--- day9.py
+++ day9.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import numpy as np
 from shared import register, main
 from utils import product
@@ -46,39 +47,35 @@ def level1(ip):
 def level2(ip):
 	hmap = parse(ip)
 	height, width = hmap.shape
-	#right, down, left, up = shifts(hmap)
-	basin = -(1 + (hmap == 9)) # -2 where there's a 9, -1 otherwise
+	basin_map = {}
+	next_basin_id = 0
 
-	basin_count = 0
-
-	# all we need to do is find the contiguous areas of 1s
 	for i in range(height):
-		for j in range(height):
-			if hmap[i, j] == 9:
+		for j in range(width):
+			if hmap[i, j] < 9:
+				basin = basin_map.get((i - 1, j), None)
+				left_basin = basin_map.get((i, j - 1), None)
 
+				if basin is None:
+					if left_basin is None:
+						basin = next_basin_id
+						next_basin_id += 1
+					else:
+						basin = left_basin
+				elif left_basin is not None and left_basin != basin:
+					# merge the basins
+					for idx, basin_at_idx in basin_map.items():
+						if basin_at_idx == left_basin:
+							basin_map[idx] = basin
 
-			# -2 = it's a peak
-			# -1 = still haven't decided what basin it's in
-			if basin[i, j] == -1:
-				# look at the top and left, if either have a basin set, that's our basin
-				if i > 0 and basin[i - 1, j] > -1:
-					basin[i, j] = basin[i - 1, j]
-				elif j > 0 and basin[i, j - 1] > -1:
-					basin[i, j] = basin[i, j - 1]
-				else:
-					# otherwise, it's a new basin
-					basin[i, j] = basin_count
-					basin_count += 1
+				basin_map[i, j] = basin
 
-	print(basin[:10, :10])
+	basin_sizes = Counter()
 
-	basin_sizes = []
+	for i, basin in basin_map.items():
+		basin_sizes[basin] += 1
 
-	for basin_id in range(basin_count):
-		basin_sizes.append(sum(hmap[basin == basin_id]))
-
-	basin_sizes.sort()
-	return product(basin_sizes[-3:])
-
+	largest_three = [size for basin, size in basin_sizes.most_common()][:3]
+	return product(largest_three)
 
 main(__name__)
