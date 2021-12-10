e97704fc5da5030a7a860a92e0b52e74dc67c5ea
Andrew Foote <footeandrew1@gmail.com>
2021-12-10 19:21:33 +0000

    day 10 level 2

--- day10.py
+++ day10.py
@@ -1,10 +1,13 @@
 from collections import Counter
 import itertools as it
 from shared import register, main
-from utils import product
+from utils import parse_digits, product
 
 OPENERS = ('(', '[', '{', '<')
 CLOSERS = (')', ']', '}', '>')
+
+def closer_for_opener(opener):
+	return CLOSERS[OPENERS.index(opener)]
 
 SCORES = {
 	')': 3,
@@ -13,27 +16,50 @@ SCORES = {
 	'>': 25137
 }
 
-@register(day=10, level=1)
-def level1(ip):
+def discard_corrupted(lines):
 	counter = Counter()
 
-	for lineno, line in enumerate(ip):
+	for line in lines:
 		opener_stack = []
 
 		for c in line.strip():
 			if c in OPENERS:
-				opener_stack.append(c)
+					opener_stack.append(c)
 			elif c in CLOSERS:
 				opener = opener_stack.pop()
-				expected_closer = CLOSERS[OPENERS.index(opener)]
+				expected_closer = closer_for_opener(opener)
 
 				if c != expected_closer:
 					counter[c] += 1
 					break
 			else:
 				assert False
+		else:
+			yield line, opener_stack
 
+	return counter
+
+@register(day=10, level=1)
+def level1(ip):
+	counter = yield from discard_corrupted(ip)
 	return sum(SCORES[closer] * count for closer, count in counter.items())
 
+@register(day=10, level=2)
+def level2(ip):
+	scores = []
+
+	for line, opener_stack in discard_corrupted(ip):
+		opener_stack.reverse()
+		closers = map(closer_for_opener, opener_stack)
+		
+		score = parse_digits(
+			(CLOSERS.index(closer) + 1 for closer in closers),
+			5
+		)
+
+		scores.append(score)
+
+	scores.sort()
+	return scores[len(scores) // 2]
 
 main(__name__)
